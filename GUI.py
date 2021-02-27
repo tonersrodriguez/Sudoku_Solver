012c62199be5bf596637920a4e5cc5c17c7058cc
Tony Rodriguez <trodriguez13983@gmail.com>
2021-02-27 02:07:23 -0800

    Created gitpod class to import requirement.txt module to load GUI application

--- GUI.py
+++ GUI.py
@@ -1,8 +1,9 @@
-import pygame 
-import time 
+import pygame
+import time
 pygame.font.init()
 
-class Grid: 
+
+class Grid:
     board = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
         [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -15,25 +16,25 @@ class Grid:
         [0, 4, 9, 2, 0, 6, 0, 0, 7]
     ]
 
-    def __init__(self, rows, cols, width, height, win): 
-        self.row = rows
-        self.cols = cols 
-        self.cubes = [[Cube(self.board[i][j], i, j, width, height)for j in range (cols)] for i in range(rows)]
-        self.width = width 
+    def __init__(self, rows, cols, width, height, win):
+        self.rows = rows
+        self.cols = cols
+        self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
+        self.width = width
         self.height = height
         self.model = None
         self.update_model()
         self.selected = None
-        self.win = win 
-    
-    def update_model(self): 
-        slef.model = [[self.cubes[i][j]. value for j in range(self.cols)] for i in range(self.rows)]
-    
+        self.win = win
+
+    def update_model(self):
+        self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
+
     def place(self, val):
         row, col = self.selected
         if self.cubes[row][col].value == 0:
             self.cubes[row][col].set(val)
-            self.update_model() 
+            self.update_model()
 
             if valid(self.model, val, (row,col)) and self.solve():
                 return True
@@ -42,69 +43,69 @@ class Grid:
                 self.cubes[row][col].set_temp(0)
                 self.update_model()
                 return False
-        
-    def sketch(self, val)
+
+    def sketch(self, val):
         row, col = self.selected
         self.cubes[row][col].set_temp(val)
 
     def draw(self):
-        #Draws the grid lines of the GUI program 
-        gap.self.width / 9
-        for i in range(self.rows+1)
-            if i % 3  == 0 and i != 0: 
-                thick = 4 
-            else: 
-                thick = 1 
+        # Draw Grid Lines
+        gap = self.width / 9
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
+                thick = 4
+            else:
+                thick = 1
             pygame.draw.line(self.win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
-            pygame.draw.ine(self.win, (0,0,0), (i * gap , 0), (i * gap, self.height), thick)
+            pygame.draw.line(self.win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
 
-        #Draw Cubes
-        for i in range(self.rows): 
-            for j in range(self.cols): 
+        # Draw Cubes
+        for i in range(self.rows):
+            for j in range(self.cols):
                 self.cubes[i][j].draw(self.win)
-    
-    def select (self, row, col): 
-        #Reset all other cubes to solve puzzle 
-        for i in range(self.rows): 
-            for j in range(self.cols): 
+
+    def select(self, row, col):
+        # Reset all other
+        for i in range(self.rows):
+            for j in range(self.cols):
                 self.cubes[i][j].selected = False
 
-            self.cubes[row][col].selected = True
-            self.selected = (row, col)
+        self.cubes[row][col].selected = True
+        self.selected = (row, col)
 
-    def clear(self): 
-        row, col = self.selected 
-        if self.cubes[row][col].value == 0: 
+    def clear(self):
+        row, col = self.selected
+        if self.cubes[row][col].value == 0:
             self.cubes[row][col].set_temp(0)
 
-    def click(self, pos): 
+    def click(self, pos):
         """
-        :param: pos 
+        :param: pos
         :return: (row, col)
         """
-        if pos[0] < self.width and pos[1] < self.height: 
-            gap = self.width / 9 
-            x = pos[0] // gap 
-            y = pos[1] // gap 
-            return (int(y), int(x))
-        else: 
+        if pos[0] < self.width and pos[1] < self.height:
+            gap = self.width / 9
+            x = pos[0] // gap
+            y = pos[1] // gap
+            return (int(y),int(x))
+        else:
             return None
-    
-    def is_finished (self): 
-        for i in range(self.rows): 
-            for j in range(self.cols):
-                if self.cubes[i][j].value == 0: 
-                    return False
-        return True 
-    
-    def solve(self: 
-        find = find_empty(self.model)
-        if not find: 
-            return True 
-        else: 
-            row, col = find 
 
-        for i in range(1, 10): 
+    def is_finished(self):
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.cubes[i][j].value == 0:
+                    return False
+        return True
+
+    def solve(self):
+        find = find_empty(self.model)
+        if not find:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
             if valid(self.model, i, (row, col)):
                 self.model[row][col] = i
 
@@ -115,15 +116,15 @@ class Grid:
 
         return False
 
-    def solve_gui(self): 
+    def solve_gui(self):
         self.update_model()
         find = find_empty(self.model)
-        if not find: 
+        if not find:
             return True
-        else: 
+        else:
             row, col = find
 
-        for i in range(1, 10): 
+        for i in range(1, 10):
             if valid(self.model, i, (row, col)):
                 self.model[row][col] = i
                 self.cubes[row][col].set(i)
@@ -144,9 +145,10 @@ class Grid:
 
         return False
 
-class Cube: 
-    rose = 9 
-    colse = 9 
+
+class Cube:
+    rows = 9
+    cols = 9
 
     def __init__(self, value, row, col, width, height):
         self.value = value
@@ -338,4 +340,4 @@ def main():
 
 
 main()
-pygame.quit()        
+pygame.quit()
